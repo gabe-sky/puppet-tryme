53083fcd26b89b66d635d382ac5d3d8c582de8f0
Gabriel M Schuyler <gabe@puppetlabs.com>
2019-09-06 17:58:53 -0500

    Fix up rspec for lint and things

--- spec/classes/tryme_spec.rb
+++ spec/classes/tryme_spec.rb
@@ -3,15 +3,9 @@ require 'spec_helper'
 describe 'tryme' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) do
-        os_facts.merge(
-          'fqdn' => 'dorothy.puppetlabs.vm',
-        )
-      end
+      let(:facts) { os_facts }
 
       it { is_expected.to compile }
-      it { pp catalogue.resources }
-      it { is_expected.to contain_notify("#{facts['fqdn']}") }
     end
   end
 end
